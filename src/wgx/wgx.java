d29881dfff10644a2ae75f76b72c4d3e24c61c66
AganWen <q1236789014@qq.com>
2018-07-18 16:28:07 +0800

    找回

--- src/wgx/wgx.java
+++ src/wgx/wgx.java
@@ -4,7 +4,8 @@ public class wgx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("adsd");
+			System.out.println("我开始修改项目");
+			System.out.println("我第二次修改");
 	}
 
 }
